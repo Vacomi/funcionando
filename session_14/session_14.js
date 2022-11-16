d9bb1b9fa608dd52eac5395653ebd499304712df
Michael Vargas <vacomi.corp@gmail.com>
2022-11-15 20:54:42 -0500

    Agregando session14

---
+++ session_14/session_14.js
@@ -0,0 +1,34 @@
+// EJERCICIO 01
+
+// let mujeres = +prompt('Ingrese cantidad de mujeres');
+// let varones = +prompt('Ingrese cantidad de varones');
+// let total = mujeres + varones;
+// console.log(total);
+// total /= 100;
+// console.log('Mujeres '+ (mujeres/total).toFixed(2) +' %');
+// console.log('Varores '+ (varones/total).toFixed(2) +' %');
+
+
+// Ejercicio 02
+
+// let inversion = +prompt('Ingrese su monto de inversion');
+
+
+// console.log(`Av. La Marj: ${inversion * 0.35}`);
+// console.log(`Av. Abancay: ${inversion * 0.25}`);
+// console.log(`Av. 28 de Julio: ${inversion * 0.1}`);
+// console.log(`Av. Aviación: ${inversion * 0.15}`);
+// console.log(`Av. Tacna: ${inversion * 0.15}`);
+
+
+// Ejercicio 03
+
+
+// let cantGen = +prompt('Cuantas de Entrada General se vendió?');
+// let cantMay = +prompt('Cuantas de Entrada para 65 < se vendio?');
+// let cantMen = +prompt('Cuantas Entradas de Menores se vendió?');
+
+// console.log(`Entrada General ${cantGen * 150} Soles`);
+// console.log(`Entrada Olds ${cantMay * 50} Soles`);
+// console.log(`Entrada Menores ${cantMen * 80} Soles`);
+

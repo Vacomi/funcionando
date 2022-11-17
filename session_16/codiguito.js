fa15082b65a3a24d20a19572aadab955b2daf9f0
Michael Vargas <vacomi.corp@gmail.com>
2022-11-17 17:35:48 -0500

    Session 15 resueltos

---
+++ session_16/codiguito.js
@@ -0,0 +1,14 @@
+// console.log(suma(5,9));
+
+// Ejercicio 01
+let restar = (a, b) => a - b;
+
+// Ejercicio 02
+let tofaren = faren => (faren - 32)  * 5/9;
+
+// Ejercicio 03
+let suma = ( x, y ) => x + y
+
+console.log(suma(9,8)+' suma de 9 + 8');
+console.log(tofaren(75)+' Faren de 75F');
+console.log(restar(12, 5 )+ ' Restamos 12 - 5 ');

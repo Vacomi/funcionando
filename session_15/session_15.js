390465f0e417441495ddff33ed9a1c221753e0cf
Michael Vargas <vacomi.corp@gmail.com>
2022-11-16 19:19:09 -0500

    Session 15 resuelto

---
+++ session_15/session_15.js
@@ -0,0 +1,75 @@
+//  Ejercicio 1
+
+// let ic = +prompt('Ingrese su importe de compra');
+// let descuento;
+// let ifinal;
+
+// if( ic >= 150 ) {
+//   descuento = ic * 0.12;
+// } else {
+//    console.log('Oe animal, pon bien pz'); 
+// }
+
+// ifinal = ic - descuento;
+// console.log('Descuento: '+ descuento);
+// console.log('Importe Final: '+ ifinal);
+
+// EJERCICIO 02
+
+// let alumno = prompt('Ingrese su nombre')
+// let eparcial = +prompt('Ingrese su nota de EP')
+// let efinal   = +prompt('Ingrese su nota de EF')
+// let ppracticas = +prompt('Ingrese su promedio de Practicas')
+
+// let promedioFinal = (eparcial + efinal + efinal +ppracticas) / 4; 
+
+// console.log('Promedio Final :' + promedioFinal);
+
+
+
+// console.log(alumno);
+
+
+// EJERCICIO 03
+
+// let trabajador  = prompt('Ingrese su nombre')
+// let sueldo      = +prompt('Ingrese su Sueldo')
+// let nroHijos    = +prompt('Ingrese el numero de hijos')
+
+// let boni        ;
+// let sueldoFinal ;
+
+// if (nroHijos > 0) {
+//     boni = 0.07 * sueldo;
+//     sueldoFinal = sueldo + boni;
+// }
+
+// console.log(`Bonificacion ${boni} soles`)
+// console.log(`Sueldo Final:  ${sueldoFinal} soles`)
+
+
+// EJERCICIO 04
+
+// let numero = +prompt('Ingrese un numero')
+// let porciento;
+// if( numero > 500 ){
+//     porciento = numero * 0.18;
+// }
+
+// console.log('El 18% de ' + numero +' es '+ porciento);
+
+
+// EJERCICIO 05
+
+// let numerito = +prompt('ingrese numero')
+
+// function hallafactorial ( n ) {
+// 	let fac = 1; 
+
+// 	for (i=1; i<=n; i++) {
+// 		fac = fac * i; 
+// 	}
+// 	return fac; 
+// }
+
+// console.log( hallafactorial(numerito));

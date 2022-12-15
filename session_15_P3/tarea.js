ee361ee93ab84edd1866bcbae7dcedc898123c02
Michael Vargas <vacomi.corp@gmail.com>
2022-12-15 17:08:20 -0500

    sesion 15 finalizado

---
+++ session_15_P3/tarea.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hola Mundo')
+})
+
+server.listen(port, hostname, () => {
+    console.log(`El servidor esta ejecuntando en http://${hostname}:${port}/`);
+
+    console.log('Michael Alvaro Vargas Condori (Vacomi)');
+})
+
+
